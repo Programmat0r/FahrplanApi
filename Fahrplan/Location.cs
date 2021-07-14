f328554227abe38c24bcd6e506d76721ab7efee9
nlang <nicolas.lang@nl-it.de>
2021-07-14 09:40:37 +0200

    Added comments for the location class.

--- Fahrplan/Location.cs
+++ Fahrplan/Location.cs
@@ -1,20 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Fahrplan
+﻿namespace Fahrplan
 {
+    using System;
+
+    /// <summary>
+    /// Defines the <see cref="Location" />.
+    /// </summary>
     public class Location
     {
-
+        /// <summary>
+        /// Gets the Name.
+        /// </summary>
         public String Name { get; private set; }
+
+        /// <summary>
+        /// Gets the Lon.
+        /// </summary>
         public long Lon { get; private set; }
+
+        /// <summary>
+        /// Gets the Lat.
+        /// </summary>
         public long Lat { get; private set; }
+
+        /// <summary>
+        /// Gets the Id.
+        /// </summary>
         public String Id { get; private set; }
 
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Location"/> class.
+        /// </summary>
+        /// <param name="name">The name<see cref="String"/>.</param>
         public Location(String name)
         {
             if (name is null)
