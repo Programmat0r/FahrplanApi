0bb7892eb20f883072f120a64e57c61bdb4c2117
Programmat0r <nicolas.lang@nl-it.de>
2021-07-14 16:24:02 +0200

    Added Unittests for the LocationApi class.

---
+++ FahrplanTests/LocationApiTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fahrplan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Fahrplan.Tests
+{
+    [TestClass()]
+    public class LocationApiTests
+    {
+        private String Token;
+        public LocationApiTests()
+        {
+            this.Token = File.ReadAllText("Token.key");
+        }
+        [TestMethod()]
+        public void Get_Test()
+        {
+
+            LocationApi locationApi = new Fahrplan.LocationApi("Frankfurt", new Authentication(this.Token));
+            var locations = locationApi.Get();
+
+            Assert.AreEqual(locations.Length, 10);
+        }
+
+        [TestMethod()]
+        public void Location_Api_Test_No_Locationname()
+        {
+
+            Assert.ThrowsException<ArgumentException>(() => new LocationApi(" ", null));
+        }
+
+        [TestMethod()]
+        public void Location_Api_Test_No_Authentication()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new LocationApi("Frankfurt", null));
+        }
+    }
+}
