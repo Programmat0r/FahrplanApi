ee35cfefee3457349aac79bf7c4a323adfbce557
nlang <nicolas.lang@nl-it.de>
2021-07-14 09:31:53 +0200

    Removed Class 1.
    Added the needed request classes for the api: ArrivalBoard, DepartureBoard, JourneyDetails, Location.

---
+++ Fahrplan/Location.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrplan
+{
+    public class Location
+    {
+
+        public String Name { get; private set; }
+        public long Lon { get; private set; }
+        public long Lat { get; private set; }
+        public String Id { get; private set; }
+
+
+        public Location(String name)
+        {
+            if (name is null)
+                throw new ArgumentNullException("Location name can't be empty");
+
+            this.Name = name;
+        }
+    }
+}
